d053eb818c45fe19eeab8557e7bb0e5859483300
hongtao_luo <luohongtao621@hotmail.com>
2026-06-02 12:19:42 +0800

    model

---
+++ Meloht.API.Gateway/HttpResponseDataModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+
+namespace Meloht.API.Gateway
+{
+    internal class HttpResponseDataModel
+    {
+        public Guid Guid { get; set; }
+        public string ContentType { get; set; }
+
+        public string Json { get; set; }
+
+        public ResultType ResultType { get; set; }
+
+        public Attachment Attachment { get; set; }
+    }
+
+    public class Attachment
+    {
+        public string[] ContentDisposition { get; set; }
+        public Guid BlobGuid { get; set; }
+    }
+
+    public enum ResultType
+    {
+        Json = 0,
+        Blob = 1,
+        Redis = 2,
+        File = 3
+    }
+}
