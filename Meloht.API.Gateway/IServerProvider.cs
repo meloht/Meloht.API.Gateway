e6010de2a15875293f95782ed998de27bd529f74
hongtao_luo <luohongtao621@hotmail.com>
2026-06-05 14:20:43 +0800

    code

---
+++ Meloht.API.Gateway/IServerProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meloht.API.Gateway
+{
+    public interface IServerProvider
+    {
+
+
+    }
+}
---
+++ Meloht.API.Gateway/ServerNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meloht.API.Gateway
+{
+    public class ServerNode
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public int Port { get; set; }
+        public string Host { get; set; }
+
+        public int Weight { get; set; }
+    }
+}
