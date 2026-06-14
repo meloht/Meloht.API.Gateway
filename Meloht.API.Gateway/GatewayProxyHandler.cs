48eb75e208d5e3068d77c1283389f3bff1ff754c
luohongtao <luohongtao621@hotmail.com>
2026-06-14 15:17:13 +0800

    using var ct = new CancellationTokenSource(_requestTimeout);

--- Meloht.API.Gateway/GatewayProxyHandler.cs
+++ Meloht.API.Gateway/GatewayProxyHandler.cs
@@ -19,18 +19,18 @@ namespace Meloht.API.Gateway
         private readonly int _requestTimeout;
         private readonly int _poolSize;
 
-        private readonly ConcurrentDictionary<Guid, TaskCompletionSource<HttpResponseMessage>> _targetRequstQueue;
+        private readonly ConcurrentDictionary<Guid, TaskCompletionSource<HttpResponseMessage>> _targetRequestQueue;
         private readonly ObjectPool<RequestModel> _requestModelPool;
 
         private readonly ILogger<GatewayProxyHandler> _logger;
 
         Channel<RequestModel> IGatewayProxy.RequestChannel => _channel;
 
-        ConcurrentDictionary<Guid, TaskCompletionSource<HttpResponseMessage>> IGatewayProxy.TargetRequestQueue => _targetRequstQueue;
+        ConcurrentDictionary<Guid, TaskCompletionSource<HttpResponseMessage>> IGatewayProxy.TargetRequestQueue => _targetRequestQueue;
 
         public GatewayProxyHandler(IConfiguration configuration, IHostApplicationLifetime lifetime, ILogger<GatewayProxyHandler> logger)
         {
-            _targetRequstQueue = new ConcurrentDictionary<Guid, TaskCompletionSource<HttpResponseMessage>>();
+            _targetRequestQueue = new ConcurrentDictionary<Guid, TaskCompletionSource<HttpResponseMessage>>();
 
             _requestTimeout = AppSettings.GetHttpRequestTimeout(configuration) * 1000;
             _poolSize = AppSettings.GetPoolSize(configuration);
@@ -38,8 +38,8 @@ namespace Meloht.API.Gateway
             _logger = logger;
 
             _channel = Channel.CreateBounded<RequestModel>(GetChannelOptions(_poolSize));
-          
-            _requestModelPool = new ObjectPool<RequestModel>(() => new RequestModel(Guid.Empty, null), maxSize: _poolSize);
+
+            _requestModelPool = new ObjectPool<RequestModel>(() => new RequestModel(Guid.Empty, null), maxSize: Environment.ProcessorCount * 2);
             lifetime.ApplicationStopping.Register(() =>
             {
                 _channel.Writer.TryComplete();
@@ -52,10 +52,11 @@ namespace Meloht.API.Gateway
             try
             {
                 var tcs = new TaskCompletionSource<HttpResponseMessage>();
-                var ct = new CancellationTokenSource(_requestTimeout);
+
+                using var ct = new CancellationTokenSource(_requestTimeout);
 
                 Guid guid = Guid.NewGuid();
-                _targetRequstQueue.TryAdd(guid, tcs);
+                _targetRequestQueue.TryAdd(guid, tcs);
                 ct.Token.Register(() => tcs.TrySetCanceled(), useSynchronizationContext: false);
 
                 var requestModel = _requestModelPool.Rent();
@@ -72,13 +73,25 @@ namespace Meloht.API.Gateway
                 _logger.LogError(ex, "Error processing request");
                 throw;
             }
-           
+
         }
 
 
         private static async Task CopyProxyHttpResponse(HttpContext context, HttpResponseMessage responseMessage)
         {
-            context.Response.ContentType = responseMessage.Content.Headers.ContentType?.ToString();
+            
+            if (responseMessage.Content.Headers.ContentType != null)
+            {
+                context.Response.ContentType = responseMessage.Content.Headers.ContentType.ToString();
+            }
+            else
+            {
+                if (responseMessage.Content.Headers.TryGetValues("Content-Type", out var values))
+                {
+                    context.Response.ContentType = string.Join(";", values);
+                }
+            }
+
             context.Response.ContentLength = responseMessage.Content.Headers.ContentLength;
 
             await responseMessage.Content.CopyToAsync(context.Response.Body);
