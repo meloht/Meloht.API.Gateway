266a316dfdfe4cd23bedc45bf196a5ea62785fa2
hongtao_luo <luohongtao621@hotmail.com>
2026-06-23 09:33:45 +0800

    ServerProviderExtensions

---
+++ Meloht.API.ServiceDiscovery.Provider.PostgreSQL/ServerProviderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Meloht.API.ServiceDiscovery.Server;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Meloht.API.ServiceDiscovery.Provider.PostgreSQL
+{
+    public static class ServerProviderExtensions
+    {
+        public static void AddServiceDiscovery(this IServiceCollection services, IConfiguration configuration)
+        {
+            ServiceCollectionExtensions.AddDatabaseConfig(services, configuration);
+            ServiceCollectionExtensions.AddHealthCheck(services, configuration);
+            services.AddSingleton<DatabaseServerData, ServerDataPostgreSQL>();
+        }
+    }
+}
